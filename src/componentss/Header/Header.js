cee5dbb00868365c8793a5d5972ffc3f7dc2eeae
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-11-24 13:52:04 +0200

    gg
    gg

--- src/componentss/Header/Header.js
+++ src/componentss/Header/Header.js
@@ -1,30 +1,45 @@
 const Header = () => {
-    return         <header id="site-header">
-    <nav class="navbar">
-        
+  return (
+    <header id="site-header">
+      <nav class="navbar">
         <section className="navbar-dashboard">
-            <div className="first-bar">
-                <a href="#">Dashboard</a>
-                <a className="button" href="#">My Pets</a>
-                <a className="button" href="#">Add Pet</a>
-            </div>
-            <div className="second-bar">
-                <ul>
-                    <li>Welcome, Pesho!</li>
-                    <li><a href="#"><i className="fas fa-sign-out-alt"></i> Logout</a></li>
-                </ul>
-            </div>
+          <div className="first-bar">
+            <a href="#">Dashboard</a>
+            <a className="button" href="#">
+              My Pets
+            </a>
+            <a className="button" href="#">
+              Add Pet
+            </a>
+          </div>
+          <div className="second-bar">
+            <ul>
+              <li>Welcome, Pesho!</li>
+              <li>
+                <a href="#">
+                  <i className="fas fa-sign-out-alt"></i> Logout
+                </a>
+              </li>
+            </ul>
+          </div>
         </section>
         <section className="navbar-anonymous">
-            <ul>
-                <li><a href="#"><i className="fas fa-user-plus"></i> Register</a></li>
-                <li><a href="#"><i className="fas fa-sign-in-alt"></i> Login</a></li>
-            </ul>
+          <ul>
+            <li>
+              <a href="#">
+                <i className="fas fa-user-plus"></i> Register
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fas fa-sign-in-alt"></i> Login
+              </a>
+            </li>
+          </ul>
         </section>
-    </nav>
-
-
-</header>
-}
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
