8b933b9963eaae12a7a8b92c0b9f0d3422e9e5a8
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-11-25 13:57:49 +0200

    goodAfternoon
    gg

---
+++ src/componentss/Dashboard/OtherPet/otherPet.js
@@ -0,0 +1,15 @@
+const OtherPet = () => {
+
+    return(
+        <li class="otherPet">
+        <h3>Name: Buddy</h3>
+        <p>Type: dog</p>
+        <p class="img"><img src="/images/dog2.png" /></p>
+        <a class="button" href="#">Details</a>
+    </li>
+    );
+
+
+}
+
+export default OtherPet;
--- src/componentss/Dashboard/dashboard.js
+++ src/componentss/Dashboard/dashboard.js
@@ -1,36 +1,12 @@
+
 const Dashboard = () => {
+
     return(
         <section id="dashboard-page" class="dashboard">
         <h1>Dashboard</h1>
         
         <ul class="other-pets-list">
-            <li class="otherPet">
-                <h3>Name: Buddy</h3>
-                <p>Type: dog</p>
-                <p class="img"><img src="/images/dog2.png" /></p>
-                <a class="button" href="#">Details</a>
-            </li>
-
-            <li class="otherPet">
-                <h3>Name: Tyson</h3>
-                <p>Type: parrot</p>
-                <p class="img"><img src="/images/parrot.png" /></p>
-                <a class="button" href="#">Details</a>
-            </li>
-
-            <li class="otherPet">
-                <h3>Name: Milo</h3>
-                <p>Type: dog</p>
-                <p class="img"><img src="/images/dog.png" /></p>
-                <a class="button" href="#">Details</a>
-            </li>
-
-            <li class="otherPet">
-                <h3>Name: Tom</h3>
-                <p>Type: cat</p>
-                <p class="img"><img src="/images/cat1.png" /></p>
-                <a class="button" href="#">Details</a>
-            </li>
+         
         </ul>
 
         <p class="no-pets">No pets in database!</p>
