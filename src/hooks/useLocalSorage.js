ac2380e7c770332fff8aac04a0edc0f15f5f241b
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-12-07 11:51:09 +0200

    goodAfternoon

--- src/hooks/useLocalSorage.js
+++ src/hooks/useLocalSorage.js
@@ -4,9 +4,13 @@ import { useState } from "react";
 const useLocalStorage = (key, initialValue) => {
 
     const [state, setState] = useState(() => {
+
+      
      let item =   localStorage.getItem(key);
 
-     return item
+     return item 
+           ? JSON.parse(item) 
+           : initialValue;
     })
 
     return [
